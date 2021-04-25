71403c7770ade2a11cca79f797e1a3bed6961275
sameer_99 <sameershrivastava46@gmail.com>
2021-04-25 21:16:08 +0530

    added jury

---
+++ src/csvScript.js
@@ -0,0 +1,58 @@
+const ObjectsToCsv = require('objects-to-csv')
+const { User } = require('./models')
+const mongoose = require('mongoose')
+// require('dotenv').config()
+mongoose
+    .connect(
+        'mongodb+srv://tecnoesis:tecnoesis@cluster0.m0nlm.mongodb.net/prod?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+        }
+    )
+    .then(() => {
+        console.log('Database connected')
+        generateCSV()
+    })
+
+    .catch((err) => console.error(err))
+
+const generateCSV = async () => {
+    try {
+        let users = await User.find()
+        // console.log(users)
+        let eventRegistration = new Object()
+        let count = 0
+        users.forEach((user) => {
+            user.registeredEvents.forEach((event) => {
+                count++
+                if (!eventRegistration.hasOwnProperty(event.eventName)) {
+                    eventRegistration[event.eventName] = new Array()
+                }
+                if (event.isTeamBased) {
+                    eventRegistration[event.eventName].push({
+                        Email: event.teamLeaderEmail,
+                        Phone: event.teamLeaderPhone,
+                    })
+                } else {
+                    eventRegistration[event.eventName].push({
+                        Email: event.participantEmail,
+                        Phone: event.participantPhone,
+                    })
+                }
+            })
+        })
+
+        let events = Object.keys(eventRegistration)
+        for (let event of events) {
+            console.log(event + ' : ', eventRegistration[event])
+            new ObjectsToCsv(eventRegistration[event]).toDisk(`${event}.csv`)
+        }
+        console.log({ count })
+    } catch (error) {
+        console.trace(error)
+    }
+}
+// generateCSV()
